5117a547cc934e7a9ef7af046b76298f3e523c69
Isaias Medina <imedina@cajapopular.gov.ar>
2022-02-07 08:06:08 -0300

    cambios en convertir para enero 2020

--- convertir.rb
+++ convertir.rb
@@ -1,5 +1,5 @@
-origen = 'Det_Sellos_Tucuman_BCO488_202110_C1.txt'
+origen = 'Det_Sellos_Tucuman_BCO488_202201_C1.txt'
 linea = open(origen).readlines
-num = "03038"
-s = linea.map{|x| x[107..111] = num.succ!; "#{x.chomp}000"}
-open("_"+origen,"w").puts(s)
+num = "0490"
+s = linea.map{|x| x[108..111] = num.succ!; "#{x.chomp}000"}
+open("resultado_"+origen,"w").puts(s)
